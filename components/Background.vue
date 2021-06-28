2af37e1819c9ead526abb658e5666e6c5a4c063c
unknown <allfallsdown20@gmail.com>
2021-06-28 21:57:52 +0700

    make it better

---
+++ components/Background.vue
@@ -0,0 +1,59 @@
+<template>
+    <div
+      class="
+        fixed
+        items-center
+        justify-center
+        hidden
+        overflow-hidden
+        md:flex
+        left-0
+        top-0
+        w-full
+        h-full
+        z-0
+      "
+    >
+      <svg viewBox="0 0 88 88" class="w-full max-w-screen-xl text-blue-200">
+        <circle fill="currentColor" cx="44" cy="44" r="15.5"></circle>
+        <circle
+          fill-opacity="0.2"
+          fill="currentColor"
+          cx="44"
+          cy="44"
+          r="44"
+        ></circle>
+        <circle
+          fill-opacity="0.2"
+          fill="currentColor"
+          cx="44"
+          cy="44"
+          r="37.5"
+        ></circle>
+        <circle
+          fill-opacity="0.3"
+          fill="currentColor"
+          cx="44"
+          cy="44"
+          r="29.5"
+        ></circle>
+        <circle
+          fill-opacity="0.3"
+          fill="currentColor"
+          cx="44"
+          cy="44"
+          r="22.5"
+        ></circle>
+      </svg>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/movie/index.vue
+++ pages/movie/index.vue
@@ -1,53 +1,8 @@
 <template>
   <div class="relative">
-     <div
-      class="
-        absolute
-        items-center
-        justify-center
-        hidden
-        overflow-hidden
-        md:flex
-        left-0
-        top-0
-        w-full
-        h-full
-        z-0
-      "
-    >
-      <svg viewBox="0 0 88 88" class="w-full max-w-screen-xl text-blue-200">
-        <circle fill="currentColor" cx="44" cy="44" r="15.5"></circle>
-        <circle
-          fill-opacity="0.2"
-          fill="currentColor"
-          cx="44"
-          cy="44"
-          r="44"
-        ></circle>
-        <circle
-          fill-opacity="0.2"
-          fill="currentColor"
-          cx="44"
-          cy="44"
-          r="37.5"
-        ></circle>
-        <circle
-          fill-opacity="0.3"
-          fill="currentColor"
-          cx="44"
-          cy="44"
-          r="29.5"
-        ></circle>
-        <circle
-          fill-opacity="0.3"
-          fill="currentColor"
-          cx="44"
-          cy="44"
-          r="22.5"
-        ></circle>
-      </svg>
-    </div>
-    <div class="px-11 mt-3 flex justify-between items-center z-50">
+     <Background />
+    <div class="z-50">
+      <div class="px-11 mt-3 flex justify-between items-center z-50">
         <Search />
         <div><Settings /></div>
     </div>
@@ -55,6 +10,7 @@
       <div v-for="(movie, index) in movies" :key="index">
         <MovieBox :movie="movie" />
       </div>
+    </div>
     </div>
   </div>
 </template>
